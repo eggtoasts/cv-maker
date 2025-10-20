91003a51cb5da0028250373bb32862df02f8ae17
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-20 16:38:30 -0400

    refactor: restructured state and props for cleaner component design - merged multiple states into a single generalInfo object - simplified sidebar props and input with updateGeneralInfo - added InputField component - renamed "address" to "email" - fixed JSX syntax, replacing "class" to "className"

--- src/App.jsx
+++ src/App.jsx
@@ -4,42 +4,43 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function MainContent() {
-  const [name, setName] = useState(" ");
-  const [phone, setPhone] = useState(" ");
-  const [address, setAddress] = useState(" ");
-  const [location, setLocation] = useState(" ");
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    address: "",
+  });
+
+  const updateGeneralInfo = (key, value) => {
+    setGeneralInfo((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   return (
     <>
-      <div class="main-content">
+      <div className="main-content">
         <Sidebar
-          name={name}
-          setName={setName}
-          phone={phone}
-          setPhone={setPhone}
-          address={address}
-          setAddress={setAddress}
-          location={location}
-          setLocation={setLocation}
+          generalInfo={generalInfo}
+          updateGeneralInfo={updateGeneralInfo}
         />
-        <ResumeSection
-          name={name}
-          phone={phone}
-          address={address}
-          location={location}
-        />
+        <ResumeSection generalInfo={generalInfo} />
       </div>
     </>
   );
 }
 
-function GeneralInfo({ name, phone, address, location }) {
+function GeneralInfo({ generalInfo }) {
+  const { name, phone, email, address } = generalInfo;
+
+  const gen = [phone, email, address];
+
   return (
-    <div class="general section">
+    <div className="general section">
       <h1>{name}</h1>
-      <div class="section-sub-info gen">
-        <h3>{phone}</h3>
-        <h3>{address}</h3>
-        <h3>{location}</h3>
+      <div className="section-sub-info gen">
+        {gen.map((g) => (g ? <h3>{g}</h3> : null))}
       </div>
     </div>
   );
@@ -47,16 +48,16 @@ function GeneralInfo({ name, phone, address, location }) {
 
 function SchoolCard() {
   return (
-    <div class="card">
-      <div class="section-sub-info main">
-        <div class="school-title title">University Name</div>
-        <div class="school-address">Orlando, FL</div>
+    <div className="card">
+      <div className="section-sub-info main">
+        <div className="school-title title">University Name</div>
+        <div className="school-address">Orlando, FL</div>
       </div>
 
-      <div class="section-sub-info">
-        <div class="degree-info">Major Type Degree Type</div>
+      <div className="section-sub-info">
+        <div className="degree-info">Major Type Degree Type</div>
 
-        <div class="year-info">Year - Year</div>
+        <div className="year-info">Year - Year</div>
       </div>
     </div>
   );
@@ -64,8 +65,8 @@ function SchoolCard() {
 
 function SchoolInfo() {
   return (
-    <div class="section">
-      <div class="section-title school">Education</div>
+    <div className="section">
+      <div className="section-title school">Education</div>
       <SchoolCard />
       <SchoolCard />
     </div>
@@ -76,29 +77,29 @@ function BulletPoint() {}
 
 function WorkCard() {
   return (
-    <div class="card">
-      <div class="section-sub-info main">
-        <div class="job-title title">Designer</div>
-        <div class="job-address">Alafaya, FL</div>
+    <div className="card">
+      <div className="section-sub-info main">
+        <div className="job-title title">Designer</div>
+        <div className="job-address">Alafaya, FL</div>
       </div>
-      <div class="section-sub-info">
-        <div class="left-info">Work Title</div>
+      <div className="section-sub-info">
+        <div className="left-info">Work Title</div>
 
-        <div class="right-info">Year - Year</div>
+        <div className="right-info">Year - Year</div>
       </div>
 
-      <div class="descriptions">
-        <div class="bullet-item">Designed ...</div>
+      <div className="descriptions">
+        <div className="bullet-item">Designed ...</div>
 
-        <div class="bullet-item">Developed ...</div>
+        <div className="bullet-item">Developed ...</div>
       </div>
     </div>
   );
 }
 function WorkInfo() {
   return (
-    <div class="section">
-      <div class="section-title">Experience</div>
+    <div className="section">
+      <div className="section-title">Experience</div>
       <WorkCard />
       <WorkCard />
     </div>
@@ -106,14 +107,14 @@ function WorkInfo() {
 }
 
 function SkillItem() {
-  return <div class="skill-item">C++</div>;
+  return <div className="skill-item">C++</div>;
 }
 
 function SkillInfo() {
   return (
-    <div class="section skill">
-      <div class="section-title">Skills</div>
-      <div class="skills-container">
+    <div className="section skill">
+      <div className="section-title">Skills</div>
+      <div className="skills-container">
         <SkillItem />
         <SkillItem />
       </div>
@@ -121,15 +122,10 @@ function SkillInfo() {
   );
 }
 
-function Resume({ name, phone, address, location }) {
+function Resume({ generalInfo }) {
   return (
-    <div class="resume">
-      <GeneralInfo
-        name={name}
-        phone={phone}
-        address={address}
-        location={location}
-      />
+    <div className="resume">
+      <GeneralInfo generalInfo={generalInfo} />
       <SchoolInfo />
       <WorkInfo />
       <SkillInfo />
@@ -137,77 +133,78 @@ function Resume({ name, phone, address, location }) {
   );
 }
 
-function ResumeSection({ name, phone, address, location }) {
+function ResumeSection({ generalInfo }) {
   return (
-    <div class="resume-section">
-      <Resume name={name} phone={phone} address={address} location={location} />
+    <div className="resume-section">
+      <Resume generalInfo={generalInfo} />
     </div>
   );
 }
 
-function Sidebar({
-  name,
-  setName,
-  phone,
-  setPhone,
-  address,
-  setAddress,
-  location,
-  setLocation,
-}) {
+function InputField({ label, propKey, generalInfo, updateGeneralInfo }) {
   return (
-    <div class="sidebar">
-      <div class="options">
-        <button class="main"> </button>
-        <button class="settings"></button>
+    <div className="input-field">
+      <label>{label}</label>
+      <input
+        type="text"
+        value={generalInfo[propKey]}
+        onChange={(e) => updateGeneralInfo(propKey, e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Sidebar({ generalInfo, updateGeneralInfo }) {
+  return (
+    <div className="sidebar">
+      <div className="options">
+        <button className="main"> </button>
+        <button className="settings"></button>
       </div>
-      <form class="sidebar-item">
-        <div class="sidebar-title"> General Information</div>
+      <form className="sidebar-item">
+        <div className="sidebar-title"> General Information</div>
 
-        <label>Full Name</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+        <InputField
+          label="Full Name"
+          propKey="name"
+          generalInfo={generalInfo}
+          updateGeneralInfo={updateGeneralInfo}
         />
-
-        <label>Phone</label>
-        <input
-          type="text"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+        <InputField
+          label="Phone"
+          propKey="phone"
+          generalInfo={generalInfo}
+          updateGeneralInfo={updateGeneralInfo}
         />
-
-        <label>Email Address</label>
-        <input
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
+        <InputField
+          label="Email Address"
+          propKey="email"
+          generalInfo={generalInfo}
+          updateGeneralInfo={updateGeneralInfo}
         />
-
-        <label>Address</label>
-        <input
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+        <InputField
+          label="Address"
+          propKey="address"
+          generalInfo={generalInfo}
+          updateGeneralInfo={updateGeneralInfo}
         />
       </form>
 
-      <form class="sidebar-item">
-        <div class="sidebar-title">
+      <form className="sidebar-item">
+        <div className="sidebar-title">
           Education <button> v</button>
         </div>
       </form>
 
-      <form class="sidebar-item">
-        <div class="sidebar-title">
+      <form className="sidebar-item">
+        <div className="sidebar-title">
           Experience
           <button> v</button>
         </div>
       </form>
 
-      <form class="sidebar-item">
-        <div class="sidebar-title">
+      <form className="sidebar-item">
+        <div className="sidebar-title">
           Skills
           <button> v</button>
         </div>
