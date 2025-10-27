bafd0444fd752ee946cf3d42d58dccc9458d12f0
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-10-27 09:29:54 -0400

    Add general info icons + tweak resume styling

--- src/components/Resume/GeneralInfo.jsx
+++ src/components/Resume/GeneralInfo.jsx
@@ -1,12 +1,33 @@
+import Icon from "@mdi/react";
+import { mdiPhone, mdiEmail, mdiMapMarker } from "@mdi/js";
+
 function GeneralInfo({ generalInfo }) {
   const { name, phone, email, address } = generalInfo;
   const gen = [phone, email, address];
+
+  function GeneralIcons(g) {
+    switch (g) {
+      case 0:
+        return <Icon className="icon" path={mdiPhone} size={0.7} />;
+      case 1:
+        return <Icon className="icon" path={mdiEmail} size={0.7} />;
+      case 2:
+        return <Icon className="icon" path={mdiMapMarker} size={0.7} />;
+    }
+  }
 
   return (
     <div className="general section">
       <h1 class="name">{name}</h1>
       <div className="section-sub-info gen">
-        {gen.map((g, i) => g && <h3 key={i}>{g}</h3>)}
+        {gen.map(
+          (g, i) =>
+            g && (
+              <h3 key={i}>
+                {GeneralIcons(i)} {g}
+              </h3>
+            )
+        )}
       </div>
     </div>
   );
